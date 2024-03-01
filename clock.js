2a9db0929b55ecc1f17c37b64d9b4c469355bf27
MoGameMaker <161384554+MoGameMaker@users.noreply.github.com>
2024-03-01 15:49:33 -0400

    first commit

---
+++ clock.js
@@ -0,0 +1,14 @@
+var timeEl = document.getElementById('time');
+
+function getTime() {
+    var currentTime = new Date().toLocaleTimeString();
+    return currentTime;
+};
+
+function updateTime() {
+    timeEl.innerHTML = getTime();
+}
+if (timeEl !== null) {
+    updateTime();
+    setInterval(updateTime, 1000);
+}
